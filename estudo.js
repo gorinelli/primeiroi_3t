307da0c559172cabeab81af392e86510b05961df
gorinelli <106194389+gorinelli@users.noreply.github.com>
2022-09-27 17:46:36 -0300

    Aula 27-09

--- estudo.js
+++ estudo.js
@@ -1,1 +1,8 @@
 
+        //alert("Oi! Eu sou o Alert")
+        //document .write ("Estou n apágina.")
+        //console . log("Eu estou no console.")
+     
+function alo(){
+    alert("Clicou no botão!")
+}
