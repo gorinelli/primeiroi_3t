e06aa8441d1a2a4351d14ebcf05bcef5348db27e
gorinelli <106194389+gorinelli@users.noreply.github.com>
2022-10-04 17:43:56 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,5 +1,6 @@
 
-       //alert("Oi! Eu sou o Alert");
+       
+ //alert("Oi! Eu sou o Alert");
  //document .write ("Estou na apágina.");
  //console . log("Eu estou no console.");
  let tabuada = 5;
@@ -16,4 +17,18 @@ function alo(){
     document.write( tabuada + " x 8 = " + (tabuada *8) + "<br>");
     document.write( tabuada + " x 9 = " + (tabuada *9) + "<br>");
     document.write( tabuada + " x 10 = " + (tabuada *10) + "<br>");
+}
+//for(inicio, validaçao, manutençao)
+function escreva(){
+   for(var i=1; i <= 10; i++){
+      for(var j = 1; j <=10; j++){
+       document.write(i  + " x " +  j  +  " = " + (j*i) +"<br>") ;
+      } 
+       document.write("<br>") ;
+    }
+}
+function quadrado(){
+    for(var i = 2; i < 11; i++){
+        document.write("O quadrado de" + i + " é "  + (i*i)+"<br>") ;
+    }
 }
