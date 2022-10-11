ea9a52941b6322b04ac346a58848bf0e49512cc9
gorinelli <106194389+gorinelli@users.noreply.github.com>
2022-10-11 17:48:08 -0300

    Update estudo.js
    Signed-off-by: gorinelli <106194389+gorinelli@users.noreply.github.com>

--- estudo.js
+++ estudo.js
@@ -39,3 +39,12 @@ function total(){
      let resultado = (val * ((ju/100)+1));
      document.write("O total é de:" + resultado);
 }
+
+function soma(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = Number(n1) + Number(n2)  + Number(n3) + Number(n4);
+    document.getElementById("resultado").innerHTML = r;
+}
